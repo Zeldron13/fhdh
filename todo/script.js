a8cc9a27a3a4a12dcbaa0963c6f490a888447935
Evgeny <zeka2111@bk.ru>
2026-09-04 21:20:00 +0300

    сохранение в localStorage

--- todo/script.js
+++ todo/script.js
@@ -2,39 +2,79 @@ const input = document.querySelector("#todoInput");
 const ul = document.querySelector("#taskList");
 const addBtn = document.querySelector("#addBtn");
 
+let tasks = loadTasks();
+renderTasks();
+
 addBtn.addEventListener("click", addTask);
 
 ul.addEventListener("click", function (event) {
+  const id = Number(event.target.closest("li").dataset.id);
+
   if (event.target.classList.contains("delete-btn")) {
-    event.target.closest("li").remove();
+    deleteTasks(id);
     return;
   }
 
-  const li = event.target.closest("li");
-  if (li) {
-    li.querySelector(".task-text").classList.toggle("completed");
-  }
+  toggleTasks(id);
 });
 
 function addTask() {
-  if (!input.value.trim()) {
+  const value = input.value.trim();
+  if (!value) {
     return;
   }
 
-  const li = document.createElement("li");
-  li.className = "task";
-
-  const span = document.createElement("span");
-  span.textContent = input.value;
-  span.className = "task-text";
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "Удалить";
-  deleteBtn.className = "delete-btn";
-
-  li.appendChild(span);
-  li.appendChild(deleteBtn);
-  ul.appendChild(li);
-
+  tasks.push({ id: Date.now(), text: value, complited: false });
   input.value = "";
+  saveTasks();
+  renderTasks();
+}
+
+function saveTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function deleteTasks(id) {
+  tasks = tasks.filter((task) => task.id !== id);
+  saveTasks();
+  renderTasks();
+}
+
+function toggleTasks(id) {
+  console.log(id);
+  const task = tasks.find((task) => task.id === id);
+  task.completed = !task.completed;
+  saveTasks();
+  renderTasks();
+}
+
+function renderTasks() {
+  ul.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const li = document.createElement("li");
+    li.className = "task";
+    li.dataset.id = task.id;
+
+    const span = document.createElement("span");
+    span.textContent = task.text;
+    span.className = "task-text";
+
+    if (task.completed) {
+      span.classList.add("completed");
+    }
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Удалить";
+    deleteBtn.className = "delete-btn";
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    ul.appendChild(li);
+  });
+}
+
+function loadTasks() {
+  const saved = localStorage.getItem("tasks");
+  return saved ? JSON.parse(saved) : [];
 }
